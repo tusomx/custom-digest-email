b80c6dbd30d98a8961fdd31e8fdccf42f668d5f2
Kudzai Tuso <tusomx@gmail.com>
2025-03-27 01:43:06 +0200

    Adds after_initialize

--- plugin.rb
+++ plugin.rb
@@ -3,4 +3,6 @@
 # version: 0.1
 # authors: Kudzai Tuso
 
-EmailController.prepend_view_path File.expand_path("../views", __FILE__)
+after_initialize do
+  EmailController.prepend_view_path File.expand_path("../views", __FILE__)
+end
