17e779c3d2ffd2f2afaff6b86c04a8db61a866cf
Kudzai Tuso <tusomx@gmail.com>
2025-03-27 01:33:23 +0200

    Update template path and file name.

--- plugin.rb
+++ plugin.rb
@@ -3,13 +3,4 @@
 # version: 0.1
 # authors: Kudzai Tuso
 
-after_initialize do
-  module ::CustomDigestEmail
-    def template_path(template)
-      return "#{Rails.root}/plugins/custom-digest-email/views/user_notifications/custom_digest" if template == "user_notifications.digest"
-      super(template)
-    end
-  end
-
-  Email::Renderer.prepend CustomDigestEmail
-end
+EmailController.prepend_view_path File.expand_path("../views", __FILE__)
