4fd5e0f6915754aae28d41f4558fbd36e39ee576
Kudzai Tuso <tusomx@gmail.com>
2025-03-26 20:40:21 +0200

    Adds after initialize module in plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,3 +2,14 @@
 # about: Customizes the digest email template
 # version: 0.1
 # authors: Kudzai Tuso
+
+after_initialize do
+  module ::CustomDigestEmail
+    def template_path(template)
+      return "#{Rails.root}/plugins/custom-digest-email/views/user_notifications/custom_digest" if template == "user_notifications.digest"
+      super(template)
+    end
+  end
+
+  Email::Renderer.prepend CustomDigestEmail
+end
